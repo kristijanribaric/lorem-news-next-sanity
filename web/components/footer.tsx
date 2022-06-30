e364b950aa930694bd60d447a72a489646d96f91
kristijanribaric <kriba13@gmail.com>
2022-06-30 10:40:56 +0200

    seo optimizstion

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -1,8 +1,8 @@
-
+import Link from "next/link";
 const Footer: React.FC = () => {
   return (
     <div className="surface-ground border-top-1 border-100  w-full m-0 py-3 flex justify-content-around">
-      <a>Lorem News | Your daily source of news</a>
+      <Link href="/">Lorem News | Your daily source of news</Link>
     </div>
   );
 };
